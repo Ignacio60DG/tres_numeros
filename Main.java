35cc3e35889c882661c80805997172d7cbfa3eea
Nacho Diez <diezgarcianacho@gmail.com>
2024-10-04 08:31:11 +0200

    Comdigo de tres numeros

--- Main.java
+++ Main.java
@@ -15,14 +15,18 @@ public class Main {
         sc.close();
 
         if(number1>number2 && number3>number2){
-            System.out.println(number1 +" > "+ number3 +">"+ number2);
+            System.out.println(number1 +" > "+ number3 +" > "+ number2);
         }else{
             if(number3>number2 && number2>number1){
-                System.out.println(number3 +" > "+ number2 +">"+ number1);
+                System.out.println(number3 +" > "+ number2 +" > "+ number1);
             }if(number2>number3 && number3>number1){
-                System.out.println(number2 +" > "+ number3 +">"+ number1);
+                System.out.println(number2 +" > "+ number3 +" > "+ number1);
             }if(number2>number3 && number1>number3){
-                System.out.println(number2 +" > "+ number1 +">"+ number3);
+                System.out.println(number2 +" > "+ number1 +" > "+ number3);
+            }if(number1==number3 && number2==number3){
+                System.out.println(number1 +" = "+ number2 +" = "+ number3);
+            }else{
+                System.out.println("Un nuero es igual a otro");
             }
         }
     }
