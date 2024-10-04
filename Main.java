7079ef4924445a1904dc68092a10dccc822d96ef
Nacho Diez <diezgarcianacho@gmail.com>
2024-10-04 08:42:19 +0200

    Comandos de mayor y menor

--- Main.java
+++ Main.java
@@ -16,17 +16,19 @@ public class Main {
 
         if(number1>number2 && number3>number2){
             System.out.println(number1 +" > "+ number3 +" > "+ number2);
+        }else if(number1>number2 && number3<number2){
+            System.out.println(number1 +" > "+ number2 +" > "+ number3);
         }else{
             if(number3>number2 && number2>number1){
                 System.out.println(number3 +" > "+ number2 +" > "+ number1);
-            }if(number2>number3 && number3>number1){
+            }else if(number2>number3 && number3>number1){
                 System.out.println(number2 +" > "+ number3 +" > "+ number1);
-            }if(number2>number3 && number1>number3){
+            }else if(number2>number3 && number1>number3){
                 System.out.println(number2 +" > "+ number1 +" > "+ number3);
-            }if(number1==number3 && number2==number3){
+            }else if(number1==number3 && number2==number3){
                 System.out.println(number1 +" = "+ number2 +" = "+ number3);
             }else{
-                System.out.println("Un nuero es igual a otro");
+                System.out.println("Hay dos numeros iguales");
             }
         }
     }
